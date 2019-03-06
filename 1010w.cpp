908fb4732a910a898a2bf810aef13cf84aeff301
i506864 <nan.cui@sap.com>
2019-03-06 17:58:58 +0800

    1010w
    3.6 daytime

---
+++ 1010w.cpp
@@ -0,0 +1,34 @@
+// poj 1010
+// 2019.3.6
+
+/* input:
+1 2 3 0
+7 4 0
+1 1 0
+6 2 3 0
+*/
+
+/* output:
+7 (3): 1 1 2 3
+4 (2): 1 3
+6 ---- none
+2 (2): 1 1
+3 (2): tie
+*/
+
+// types <= 25
+// won't sell more than 4 stamps at a time
+
+// number of types should be max
+// as few duplicate stamps as possible
+// these count as different types, even though they are the same denomination
+// tie: the fewest total stamps > the highest single-value stamp > "tie"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}

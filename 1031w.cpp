f3ac5558a6ffb0d726e349cc38c42700f05325d2
ChrisCN97 <874147338@qq.com>
2019-03-19 22:33:02 +0800

    1031w
    3.19 night

---
+++ 1031w.cpp
@@ -0,0 +1,24 @@
+// poj 1031
+// 2019.3.19
+
+/* input:
+0.5 1.7 3
+1.0 3.0
+2.0 -1.0
+-4.0 -1.0
+*/
+
+/* output:
+5.34
+*/
+
+// I = k * h * (a1 - a2)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+
+	return 0;
+}

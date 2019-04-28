7be2437388fc7efcfb040b58a8dd18d3252da883
i506864 <nan.cui@sap.com>
2019-04-28 12:55:00 +0800

    Update 1041w.cpp

--- 1041w.cpp
+++ 1041w.cpp
@@ -2,6 +2,12 @@
 // 2019.4.27
 
 /* input:
+1 1 1
+0 0
+1 2 1
+2 2 2
+2 1 3
+0 0
 1 2 1
 2 3 2
 3 1 6
@@ -22,6 +28,7 @@
 3 1 4
 1 2 7
 2 3 8
+3 1 12
 1 2 11
 2 3 9
 3 1 10
@@ -44,13 +51,15 @@ Round trip does not exist.
 #include <vector>
 using namespace std;
 
-int map[45][45];
-int route[1996][2], rTime, rNum, rBegin;
-bool check[1996];
+const int maxe = 2018;
+
+int map[60][maxe];
+int route[maxe][2], rTime, rNum, rBegin;
+bool check[maxe];
 vector<int> result;
 
 void fillRoute(int p, int r) {
-	for (int n = 0; n < 45; n++) {
+	for (int n = 0; n < maxe; n++) {
 		if (map[p][n] == 0) {
 			map[p][n] = r;
 			return;
@@ -59,44 +68,26 @@ void fillRoute(int p, int r) {
 }
 
 int countRoute(int p) {
-	for (int n = 0; n < 45; n++) {
+	for (int n = 0; n < maxe; n++) {
 		if (map[p][n] == 0)
 			return n;
 	}
 }
 
-bool dfs(int p) {
+void dfs(int p, int r) {
 	for (int n = 0; n < countRoute(p); n++) {
 		if (check[map[p][n]])
 			continue;
 		check[map[p][n]] = true;
-		result.push_back(map[p][n]);
-		rTime++;
 		int another;
 		if (route[map[p][n]][0] == p)
 			another = route[map[p][n]][1];
 		else
 			another = route[map[p][n]][0];
-		if (rTime == rNum) {
-			if (another == rBegin)
-				return true;
-			else {
-				result.pop_back();
-				check[map[p][n]] = false;
-				rTime--;
-				continue;
-			}
-		}
-		
-		if (dfs(another))
-			return true;
-		else {
-			result.pop_back();
-			check[map[p][n]] = false;
-			rTime--;
-		}
+
+		dfs(another, map[p][n]);
 	}
-	return false;
+	result.push_back(r);
 }
 
 int main() {
@@ -106,19 +97,19 @@ int main() {
 		if (p1 + p2 == 0)
 			break;
 		cin >> r;
-		for (int m = 0; m < 45; m++) {
-			for (int n = 0; n < 45; n++) {
+		for (int m = 0; m < 60; m++) {
+			for (int n = 0; n < maxe; n++) {
 				map[m][n] = 0;
 			}
 		}
-		for (int m = 0; m < 1996; m++) {
+		for (int m = 0; m < maxe; m++) {
 			check[m] = false;
 			for (int n = 0; n < 2; n++) {
 				route[m][n] = 0;
 			}
 		}
-		map[p1][0] = r;
-		map[p2][0] = r;
+		fillRoute(p1, r);
+		fillRoute(p2, r);
 		route[r][0] = p1;
 		route[r][1] = p2;
 		rBegin = p1 < p2 ? p1 : p2;
@@ -135,22 +126,27 @@ int main() {
 			route[r][1] = p2;
 			rNum++;
 		}
-		for (int n = 0; n < 45; n++) {
-			if (map[n][0] != 0) {
-				sort(map[n], map[n] + countRoute(n));
+		int nIn;
+		for (nIn = 0; nIn < 60; nIn++) {
+			if (map[nIn][0] != 0) {
+				if (countRoute(nIn) % 2 != 0)
+					break;
+				sort(map[nIn], map[nIn] + countRoute(nIn));
 			}
+		}
+		if (nIn != 60) {
+			cout << "Round trip does not exist." << endl;
+			continue;
 		}
 		rTime = 0;
-		if (dfs(rBegin)) {
-			for (int n = 0; n < rNum; n++) {
-				cout << result[n] << " ";
-			}
-			cout << endl;
+		dfs(rBegin, 0);
+		for (int n = rNum - 1; n > 0; n--) {
+			cout << result[n] << " ";
 		}
-		else {
-			cout << "Round trip does not exist." << endl;
-		}
+		cout << result[0];
+		cout << endl;
+		
 	}
-	
+
 	return 0;
 }
